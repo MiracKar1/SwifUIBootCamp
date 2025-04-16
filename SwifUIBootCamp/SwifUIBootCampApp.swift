b0982f47e5a45f4e5cff2ff0e7f8542b56137e4b
MiracKar.1 <flexdeneme@gmail.com>
2025-04-16 19:54:29 +0300

    Initial Commit

---
+++ SwifUIBootCamp/SwifUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIBootCampApp.swift
+//  SwifUIBootCamp
+//
+//  Created by Mirac Kar on 16.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwifUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwifUIBootCampTests/SwifUIBootCampTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIBootCampTests.swift
+//  SwifUIBootCampTests
+//
+//  Created by Mirac Kar on 16.04.2025.
+//
+
+import Testing
+@testable import SwifUIBootCamp
+
+struct SwifUIBootCampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
